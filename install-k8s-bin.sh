afb3fa01fc9b6d6e96546558b748cd542d8924c0
raydigitallife <digitallife.mailbox@gmail.com>
2018-09-04 16:31:32 +0800

    add files

---
+++ install-k8s-bin.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# install latest kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# install kops
+wget -O kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x ./kops
+sudo mv ./kops /usr/local/bin/
+
+# install heptio-authenticator-aws
+wget -O heptio-authenticator-aws \ 
+https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.3.0/heptio-authenticator-aws_0.3.0_linux_amd64
+
+chmod +x ./heptio-authenticator-aws
+sudo mv .heptio-authenticator-aws /usr/local/bin/
+
